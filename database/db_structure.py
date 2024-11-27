c2023e6d0ea89e2d0d3715c506aff5000d0d3614
otalco <joaoguilherme.ferreira14@gmail.com>
2024-11-26 21:33:15 -0300

    connection pgdb

--- database/db_structure.py
+++ database/db_structure.py
@@ -1,11 +1,8 @@
 from sqlalchemy import create_engine, text
 import os
 
-# Configuração do Wallet
-os.environ['TNS_ADMIN'] = r'C:/Users/FN84/wallet_oracle'  # Substitua pelo caminho correto
-
 # URL de conexão com o banco
-DATABASE_URL = "oracle+oracledb://ADMIN:Marilia%401992@g7ada16f6dc1ad3_petdatabase_high.adb.oraclecloud.com"
+DATABASE_URL = "postgresql+psycopg2://username:password@localhost:5432/petdatabase"
 
 # Criar engine do SQLAlchemy
 engine = create_engine(DATABASE_URL)
@@ -13,7 +10,7 @@ engine = create_engine(DATABASE_URL)
 # Testar conexão e executar consulta
 with engine.connect() as connection:
     # Usar text para consultas SQL diretas
-    stmt = text("SELECT 'Conexão bem-sucedida!' AS resultado FROM dual")
+    stmt = text("SELECT 'Conexão bem-sucedida!' AS resultado")
     result = connection.execute(stmt)
     for row in result:
         # Acessar resultados corretamente
